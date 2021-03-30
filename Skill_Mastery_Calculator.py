9482aba87e10c5ee0251ca252c0fe11eb7cf3ed4
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-30 19:14:08 +0200

    Update Skill_Mastery_Calculator.py

--- Skill_Mastery_Calculator.py
+++ Skill_Mastery_Calculator.py
@@ -1,13 +1,20 @@
-########################################################################################################################
+###################################################################################################
 
-# first build - 2nd aug 2020
+###################################################################################################
+
+# 1st build - 2nd aug 2020
+# 2nd build - 30th mar 2021
 # Mastery Calculator Application
 
-########################################################################################################################
+###################################################################################################
 
 
 
-########################################################################################################################
+###################################################################################################
+
+print(' ')
+print(' |----------------------  MASTERY CALCULATOR ----------------------| ')
+print(' ')
 
 # get information about mastery type, hours accumulated, years since started etc
 mastery_name = input('\n'
@@ -22,7 +29,7 @@ weekly_mastery_addition = float(input('How many hours do you spend\n'
                                     'on the subject weekly?:\n'))
 
 
-########################################################################################################################
+###################################################################################################
 
 # take data on how many past years have been devolved towards the mastery
 # take (number of years) x (past weekly hours * weeks in a year) to get the
@@ -38,8 +45,18 @@ yearly_hours_percentage = int(yearly_hours * 100 / 10000)
 percentage_to_completion = int((past_hours * 100) / 10000)
 percentage_left = int(100 - percentage_to_completion)
 
+# how long it will take to reach 10k at current rate
+endGoal = 10000
+counter = 0
+totalHours = yearly_hours + past_hours
 
-########################################################################################################################
+while (endGoal > 0):
+    counter += 1
+    endGoal -= totalHours
+    # print("end goal: " + str(endGoal))
+    # print(counter)
+
+###################################################################################################
 
 # print all results
 print(' ')
@@ -62,22 +79,45 @@ print(' ')
 print('--------------------------------------')
 
 # percentage to mastery and percentage left to master
-print('Current Track to Mastery..\n'
-      'Percentage of Mastery:', percentage_to_completion, '%')
-print('Percentage Left:', percentage_left, '%')
+
+if (percentage_to_completion < 100):
+    print('Current Track to Mastery..\n'
+          'Percentage of Mastery:', percentage_to_completion, '%')
+    print('Percentage Left:', percentage_left, '%')
+    print(' ')
+    # percentage after 1,2,3,4 years
+    # after 1 year
+    print('After 1 more year..\n'
+          'Percentage of Mastery:',(percentage_to_completion + yearly_hours_percentage), '%\n'
+          'Percentage Left:', (100 - (percentage_to_completion + yearly_hours_percentage)), '%\n'
+          '\n'
+          f'At the current rate you will have reached 10.000 hours in {counter} years.\n'
+          'KEEP AT IT!')
+    print(' ')
+    # here we put the count down to how long it'll take to reach 10k at current rate
+    # insert code here
+else:
+    print('You Already reached 10.000 hours! \n'
+    'CONGRATULATIONS!')
+    print(' ')
+
+print('--------------------------------------')
+
 print(' ')
-print('--------------------------------------')
-
-
-########################################################################################################################
-
-# percentage after 1,2,3,4 years
-# after 1 year
-print('After 1 more year..\n'
-      'Percentage of Mastery:',(percentage_to_completion + yearly_hours_percentage), '%\n'
-      'Percentage Left:', (100 - (percentage_to_completion + yearly_hours_percentage)), '%')
+print(' |----------------------  MASTERY CALCULATOR ----------------------| ')
 print(' ')
-print('--------------------------------------')
-print('--------------------------------------')
 
-########################################################################################################################
+###################################################################################################
+
+# print(' ')
+# print(' |----------------------  DEBUG ----------------------| ')
+# print(' ')
+#
+#
+#
+# print(' ')
+# print(' |----------------------  DEBUG ----------------------| ')
+# print(' ')
+
+###################################################################################################
+###################################################################################################
